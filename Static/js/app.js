481965d5cdaa24c3627a8c14a01ff99a9917f0e9
nrpineda <new.lifenrpn@gmail.com>
2023-01-10 23:49:30 -0800

    Buildt the table when the page loads

--- Static/js/app.js
+++ Static/js/app.js
@@ -18,3 +18,29 @@ function buildTable(data) {
     });
 }
 
+// Creating the handleClick function
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //Check to see if a date was entered and filter the
+    //data using that table
+    if (date) {
+        //Apply 'filter' to the table data to only keep the
+        //rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //Rebuild the table using the filtered data
+    //@NOTE: If no date was entered, then the filteredData will
+    //just be the original tableData/
+    buildTable(filteredData);
+};
+//Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
+
+
+
